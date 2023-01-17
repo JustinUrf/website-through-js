5d784967ee37d349e0c7c5043a78906feb138ed6
Justin Lee <lee.justin001126@gmail.com>
2023-01-17 09:51:55 -0800

    added onload function that has variables set to create elements.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  const pElement = document.createElement("p");
+  const h1Element = document.createElement("h1");
+  const h2Element = document.createElement("h2");
+  const ulElement = document.createElement("ul");
+  const liElement = doucment.createElement("li");
+  const aElement = document.createElement("a")
+  aElement.href = "https://en.wikipedia.org/wiki/Multicoloured_tanager"
+  const imgElement = document.createElement("img");
+  imgElement.src = "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900" 
+  imgElement.alt= "This is an image of a Multicolored Tanager bird from birdsoftheworld.org"
+  append(h1) = "Webpage Recreation Practice"
+}
